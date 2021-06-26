32d561286fde1e55741f24ff5ab1e8f0334086cf
Diego Henrique <diegohrp20@gmail.com>
2021-06-26 20:15:06 -0300

    Desenvolvendo API WebService Rest para o resource Cliente e ClienteRepository.

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     use HasFactory;
+    protected $fillable = ['nome'];
+
+    public function rules()
+    {
+        return [
+            'nome' => 'required|min:3'
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório!',
+            'nome.min' => 'O nome deve ter no mínimo 3 caracteres.'
+        ];
+    }
 }
